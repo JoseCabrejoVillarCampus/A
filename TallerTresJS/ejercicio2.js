c39608ba68fc71f32c25ffbc6f2b76101a847c3f
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-14 07:08:46 -0500

    feat: :rocket: EjerciciosTallerJs3
    Inicio del taller 3 de JS

---
+++ TallerTresJS/ejercicio2.js
@@ -0,0 +1,6 @@
+/* 2. Consultas: Usando Destructuring,
+2.1 De los trainers, reportar si la asignatura (tecnología) es
+remota o presencial y de los campers el nombre de salón.
+2.2 El teléfono de la sede de Medellín y la dirección de la
+sede de Bucaramanga
+2.3 De la asignatura (tecnología) si tiene sandbox o no */
---
+++ TallerTresJS/ejercicio3.js
@@ -0,0 +1,5 @@
+/* 3. Consultas: Usando sintaxis de punto.
+3.1 Reportar, prerequisito de la asignatura (tecnología) y nro de
+créditos del roadmap
+3.2 Agregar mas objetos con mas objetos anidados de manera libre
+(por lo menos 7) */
