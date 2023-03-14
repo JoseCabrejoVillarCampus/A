1ff99dd8f54fadcaa1f903d3c06ff4cba20352a8
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-14 07:10:39 -0500

    feat: :rocket: Inicio del taller 4 js

---
+++ TallerCuatroJs/main.js
@@ -0,0 +1,22 @@
+/* 1. A través del siguiente taller, construir un menú en una función
+alert que gestione las siguientes opciones de menú referentes al
+manejo de los arrays, objetos, funciones y otras operaciones en
+JavaScript entre dichos temas. */
+
+alert(`-----MENU-----
+1. Lectura de datos
+2. Crear Objeto
+3. Mostrar Objeto
+4. Crear Array
+5. Mostrar Array
+6. Elimine primer elemento del array
+7. Elimine ultimo elemento del array
+8. Eliminar cualquier elemento del array
+9. Agregar elemento al comienzo del array
+10. Agregar elemento la final del array
+11. Crear array con objetos
+12. Iterar array con objetos con for
+13. Iterar array con objetos con ForEach
+14. Iterar array con objetos con Map y crear copia
+15. proceso personal
+0. Salir`);
