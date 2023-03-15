892069bacb54b99bb37ae367a2519b54dace7c91
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-15 09:14:30 -0500

    feat: :sparkles: Taller 3, punto 4 en construccion

---
+++ TallerTresJS/ejercicio1_1.js
@@ -0,0 +1,78 @@
+/* 1. Construir un objeto literal "campus" que gestione
+la info(PROPIEDADES) de Campus, trainers, campers, niveles,
+tecnologías, teams y roadMap
+1.1. De campus administrar los datos de contacto de las sedes en
+Bucaramanga, Bogotá, Medellín y México
+1.2. De los trainers y campers, su nombre, sus teléfonos, teams
+(horarios de las teams=> día, hora y salones (nro y piso), y el
+email, y de los campers también horarios de inglés y ser.
+1.3. De los campers, también gestionar su nivel actual, como su
+barrio y medio de transporte
+1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
+es electiva u obligatoria
+1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas */
+
+let myFormularioCampus = document.querySelector("#myFormularioCampus");
+let myFormularioCampers = document.querySelector("#myFormularioCampers");
+let myFormularioTrainers = document.querySelector("#myFormularioTrainers");
+let myFormularioNiveles = document.querySelector("#myFormularioNiveles");
+let myFormularioRoadmap = document.querySelector("#myFormularioRoadmap");
+let campus = {};
+
+myFormularioCampus.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    campus[`${data.nombreSede}`] = {Roadmap: [], Niveles: [], Camper: [], Trainers: []};
+    listaSedes();
+    myFormularioCampus.reset();
+})
+
+let listaSedes = ()=>{
+    let opciones = document.querySelector("[name='sede']");
+    opciones.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opciones.insertAdjacentHTML("beforeend", `
+            <option value="${val}">${val}</option>
+        `);
+    }
+}
+myFormularioRoadmap.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Roadmap"].unshift(data);
+    console.log(campus);
+    myFormularioRoadmap.reset();
+})
+myFormularioNiveles.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede1 = data.sede;
+    delete data.sede;
+    campus[`${sede1}`]["Niveles"].unshift(data);
+    console.log(campus);
+    myFormularioNiveles.reset();
+})
+myFormularioCampers.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede2 = data.sede;
+    delete data.sede;
+    campus[`${sede2}`]["Camper"].unshift(data);
+    console.log(campus);
+    myFormularioCampers.reset();
+})
+myFormularioTrainers.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede3 = data.sede;
+    delete data.sede;
+    campus[`${sede3}`]["Trainer"].unshift(data);
+    console.log(campus);
+    myFormularioTrainers.reset();
+})
